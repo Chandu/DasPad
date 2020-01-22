9a521448b6433a559270295ba5c2cc8a5fbdc04e
Chandu <Chandra.Vedantham@Gmail.com>
2020-01-22 12:58:32 +0530

    Copies the Source Code for the Design Classes from LeetCode Discussions - Was Lazy to type all the code :)

---
+++ DasPad.Specs/LeetCodeCollectionsSpecs/EasySpecs/DesignSpecs/MinStackSpecs.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace DasPad.Specs.LeetCodeCollectionsSpecs.EasySpecs.DesignSpecs
+{
+  internal class MinStackSpecs
+  {
+    /*
+     * https://leetcode.com/explore/interview/card/top-interview-questions-easy/98/design/562/
+     * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+      push(x) -- Push element x onto stack.
+      pop() -- Removes the element on top of the stack.
+      top() -- Get the top element.
+      getMin() -- Retrieve the minimum element in the stack.
+
+      Example:
+
+      MinStack minStack = new MinStack();
+      minStack.push(-2);
+      minStack.push(0);
+      minStack.push(-3);
+      minStack.getMin();   --> Returns -3.
+      minStack.pop();
+      minStack.top();      --> Returns 0.
+      minStack.getMin();   --> Returns -2.
+     */
+
+    public class MinStack
+    {
+      private Node head;
+
+      public void Push(int x)
+      {
+        if (head == null)
+          head = new Node(x, x);
+        else
+          head = new Node(x, Math.Min(x, head.min), head);
+      }
+
+      public void Pop()
+      {
+        head = head.next;
+      }
+
+      public int Top()
+      {
+        return head.val;
+      }
+
+      public int GetMin()
+      {
+        return head.min;
+      }
+
+      internal class Node
+      {
+        public int val;
+        public int min;
+        public Node next;
+
+        internal Node(int val, int min) : this(val, min, null)
+        {
+        }
+
+        internal Node(int val, int min, Node next)
+        {
+          this.val = val;
+          this.min = min;
+          this.next = next;
+        }
+      }
+    }
+  }
+}
---
+++ DasPad.Specs/LeetCodeCollectionsSpecs/EasySpecs/DesignSpecs/ShuffleAnArraySpecs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DasPad.Specs.LeetCodeCollectionsSpecs.EasySpecs.DesignSpecs
+{
+  public class ShuffleAnArraySpecs
+  {
+    /*
+     * Shuffle a set of numbers without duplicates.
+
+        Example:
+
+        // Init an array with set 1, 2, and 3.
+        int[] nums = {1,2,3};
+        Solution solution = new Solution(nums);
+
+        // Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+        solution.shuffle();
+
+        // Resets the array back to its original configuration [1,2,3].
+        solution.reset();
+
+        // Returns the random shuffling of array [1,2,3].
+        solution.shuffle();
+     */
+
+    public class Solution
+    {
+      public Solution(int[] nums)
+      {
+        this.nums = nums;
+        backup = nums.Clone() as int[];
+      }
+
+      private int[] backup;
+      private int[] nums;
+
+      /** Resets the array to its original configuration and return it. */
+
+      public int[] Reset()
+      {
+        nums = backup.Clone() as int[];
+        return nums;
+      }
+
+      private void Swap(int i, int j)
+      {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      }
+
+      /** Returns a random shuffling of the array. */
+
+      public int[] Shuffle()
+      {
+        var random = new Random();
+        for (var i = 0; i < nums.Length; i++)
+        {
+          Swap(i, random.Next(i, nums.Length));
+        }
+        return nums;
+      }
+    }
+  }
+}
